5256ffdc40b98b912abc9566c0668ec95a629a28
Paulus Schoutsen <balloob@gmail.com>
2020-07-02 15:54:57 -0700

    Add print data command

---
+++ examples/print-data.ts
@@ -0,0 +1,34 @@
+// Print data
+// deno run --allow-net --allow-read https://raw.githubusercontent.com/balloob/home-assistant-deno/master/examples/print-data.ts
+import { parse } from "https://deno.land/std@0.54.0/flags/mod.ts";
+import {
+  getConnection,
+  subscribeEntities,
+  HassEntities,
+} from "https://raw.githubusercontent.com/balloob/home-assistant-deno/master/mod.ts";
+
+const TYPES: { [key: string]: string } = {
+  states: "get_states",
+  config: "get_config",
+  services: "get_services",
+  user: "auth/current_user",
+  area_registry: "config/area_registry/list",
+  device_registry: "config/device_registry/list",
+  entity_registry: "config/entity_registry/list",
+};
+
+const args = parse(Deno.args);
+
+if (!args.data || TYPES[args.data] === undefined) {
+  console.error("Specify data type --data <type>");
+  console.error("Valid types are", Object.keys(TYPES).join(", "));
+  Deno.exit(1);
+}
+
+const conn = await getConnection();
+
+const result = await conn.sendMessagePromise({ type: TYPES[args.data] });
+
+console.log(JSON.stringify(result, undefined, 2));
+
+conn.close();
